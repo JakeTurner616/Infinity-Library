64658636ab8e118250ca8c5d0af035a6989dd3cb
JakeTurner616 <jake@serverboi.org>
2024-01-06 19:03:04 -0700

    Create launch-macOS.sh

---
+++ launch-macOS.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set the current directory as the base for relative paths
+BASEDIR=$(dirname "$0")
+
+# Convert BASEDIR to an absolute path (macOS compatible)
+BASEDIR=$(cd "$BASEDIR"; pwd)
+
+# Classpath including the jsoup library and your application's jar file
+CLASSPATH="$BASEDIR/lib/jsoup-1.17.2.jar:$BASEDIR/prod.jar"
+
+# Main class of your application
+MAIN_CLASS="LibGenSearchApp"
+
+# Run the application
+java -cp "$CLASSPATH" $MAIN_CLASS
